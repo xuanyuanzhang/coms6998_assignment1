3a8583a50b378697d830f31af5dcf43b902b7463
Xinyuan Zhang <zhangxinyuan1995@gmail.com>
2018-03-30 23:35:34 -0400

    change api

--- apigClient.js
+++ apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://asjgta04n6.execute-api.us-east-2.amazonaws.com/beta';
+    var invokeUrl = 'https://452sm1xbb9.execute-api.us-east-1.amazonaws.com/beta';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
