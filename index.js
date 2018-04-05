9a48f41cf088adf954ff7c6db0f4b7fa9f994ce7
Linli <lw2780@columbia.edu>
2018-04-05 13:43:41 -0400

    complete

--- index.js
+++ index.js
@@ -2,57 +2,63 @@ var util = require('util')
 var http = require('http')
 var AWS = require("aws-sdk")
 var lexruntime = new AWS.LexRuntime()
-
-
+var myId = "myBot"
 
 exports.handler = (event, context, callback) => {
     // TODO implement
     // console.log(util.inspect(event, false, null))
     // console.log(util.inspect(context, false, null))
-    // var params = {
-//   botAlias: 'STRING_VALUE', /* required */
-//   botName: 'DiningBot', /* required */
-//   inputText: 'STRING_VALUE', /* required */
-//   userId: 'STRING_VALUE', /* required */
-//   requestAttributes: {
-//     '<String>': 'STRING_VALUE',
-//     /* '<String>': ... */
-//   },
-//   sessionAttributes: {
-//     '<String>': 'STRING_VALUE',
-//     /* '<String>': ... */
-//   }
-// };
-// lexruntime.postText(params, function(err, data) {
-//   if (err) console.log(err, err.stack); // an error occurred
-//   else     console.log(data);           // successful response
-// });
-    
     var msg = event["messages"][0]["unstructured"]["text"]
-    var reply = ""
-    if(msg=="Hello") {
-      reply = "Hi, how can I help you?"
-    }
-    else if (msg=="Where should I travel?") {
-      reply ="Go to hell!!"
-    }
-    else if(msg=="Thank you") {
-      reply = "You are welcome!"
-    }
-    else {
-      reply = "Sorry, I cannot help you with that."
-    }
-    var response =    {
+    // var msg = event.messages.unstructured.text
+    var sessionAttributes = {}
+    var res = {}
+    const callLex = (res) => {
+      return new Promise((resolve, reject) => {
+        var params = {
+          botAlias: '$LATEST', /* required */
+          botName: 'DiningBot', /* required */
+          inputText: msg, /* required */
+          userId: myId, /* required */
+          sessionAttributes: sessionAttributes
+        };
+        
+        lexruntime.postText(params, function(err, data) {
+          // console.log(data);
+          if (err) {
+            reject(err);
+            // console.log(err, err.stack);
+          }
+          else {
+            resolve(data); // successful response
+            // console.log(data);
+          }
+        });
+      });
+    };
+    
+    callLex(res)
+      .then((lexResponse) => {
+        /* VALID RESPONSE */
+        var response =    {
                           "messages": [
                             {
                               "type": "string",
                               "unstructured": {
                                 "id": "bot",
-                                "text": reply,
+                                "text": lexResponse.message,
                                 "timestamp": new Date()
+                                // "timestamp": "2016-01-01"
                               }
                             }
                           ]
                         }
-    callback(null, response);
+        console.log(lexResponse.message)
+        callback(null, response); // this is the Lambda callback
+      })
+      .catch((error) => {
+        /* SOMETHING WENT WRONG */
+        console.log(error)
+        callback(error); // this is the Lambda callback
+      });
+    
 };
